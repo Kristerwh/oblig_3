537bba6278306e8f8b2c37f12238eb37b1d2c55c
Kristerwh <111729904+Kristerwh@users.noreply.github.com>
2023-10-25 16:12:55 +0200

    Tester at actions viser feil

--- script/functions.js
+++ script/functions.js
@@ -1,5 +1,5 @@
 function isLeapYear(number) {
-    return number % 4 === 0 && number % 100 !== 0 || number % 400 === 0;
+    return number % 4 === 0 && number % 100 !== 1 || number % 400 === 0;
 }
 
 module.exports = {isLeapYear : isLeapYear};
